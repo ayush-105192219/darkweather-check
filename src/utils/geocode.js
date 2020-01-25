74126814c5e2f2bd51aa631f918e44cddd3a0a58
Ayush_Gayatri <mynameayush100@gmail.com>
2020-01-25 12:11:11 +0530

    added file with gitignore

---
+++ src/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYXNoY29kZSIsImEiOiJjazVtZ2R2b3owbHpzM21uejUzZTZmem83In0.e7OfIsZnh54NDYr4K582bg'
+   
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
